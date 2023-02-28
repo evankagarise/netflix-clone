1c056af013f446983890a453d661a5eb72a71b8f
evan <95433637+evankagarise@users.noreply.github.com>
2023-02-28 13:45:13 -0500

    row completed

---
+++ src/pages/Home.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Main from '../components/Main'
+import Row from '../components/Row'
+import requests from '../Requests'
+
+
+
+
+const Home = () => {
+  return (
+    <div>
+        <Main />
+        <Row title='Up Coming' fetchUrl={requests.requestUpcoming} />
+        <Row title='Popular' fetchUrl={requests.requestPopular} />
+        <Row title='Trending' fetchUrl={requests.requestTrending} />
+        <Row title='Top Rated' fetchUrl={requests.requestTopRated} />
+        <Row title='Horror' fetchUrl={requests.requestHorror} />
+    </div>
+  )
+}
+
+export default Home
